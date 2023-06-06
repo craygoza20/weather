caab4700e3f9db5212cbff75e06bb4819dbe19a1
craygoza20 <74159984+craygoza20@users.noreply.github.com>
2023-06-05 21:28:16 -0700

    Added new funcs to weather_api and improved gui
    why is ttk so unwieldly pls help

--- gui.py
+++ gui.py
@@ -1,33 +1,93 @@
 import tkinter as tk
 import ttkbootstrap as ttk
+import weather_api as wx
+from PIL import Image, ImageTk
 
-class WeatherApp(tk.Tk):
+
+class WeatherApp(ttk.Window):
     def __init__(self):
-        super().__init__()
+        super().__init__(themename='morph')
         self.title("Weather App")
         self.geometry("1024x768")
-        
+        self.resizable(False,False)
+        self.bg = tk.PhotoImage(file='bg.png')
+        self.bg_label = ttk.Label(self, image=self.bg)
+        self.bg_label.place(x=-2,y=-2)
+        self.forecast_response = wx.forecast_api_call()
+        self.image_paths = wx.get_condition_image(self.forecast_response)
+        self.location = self.forecast_response['location_dict']['loc_name'] + ', ' + self.forecast_response['location_dict']['loc_region']
+        self.current_time = self.forecast_response['location_dict']['loc_time']
+        self.current_dict = self.forecast_response['current_dict']
+        self.day1_dict = self.forecast_response['day_1_dict']
+        self.day2_dict = self.forecast_response['day_2_dict']
         self.create_widgets()
 
+
     def create_widgets(self):
-        self.title_label = ttk.Label(self, text="WEATHER")
-        self.title_label.pack()
 
-        self.weather_frame = ttk.Frame(self)
-        self.forecast = ttk.Label(self.weather_frame, text='FORECAST HERE')
-        self.forecast.pack()
-        self.weather_frame.pack()
+        self.title_label = ttk.Label(self, text="3-Day Forecast", font=('Arial',48))
+        self.title_label.pack(pady=(180,10))
 
-        self.conditions_frame = ttk.Frame(self)
-        self.conditions = ttk.Label(self.conditions_frame, text='CURRENT WEATHER CONDITIONS')
-        self.conditions.pack()
-        self.conditions_frame.pack()
+        self.forecast_frame = ttk.Frame(self, borderwidth=1, relief='solid')
+        self.create_weather_icons()
+        self.attach_forecast_data()
+        self.forecast_frame.pack()
 
-        self.get_weather_button = ttk.Button(self, text="Get Weather")
-        self.get_weather_button.pack()
+        # I guess I can show more useful data in current conditions?
+        # self.conditions_frame = ttk.Frame(self)
+        # self.conditions = ttk.Label(self.conditions_frame, textvariable=self.current_dict)
+        # self.conditions.pack()
+        # self.conditions_frame.pack()
 
-        self.weather_label = ttk.Label(self, text="")
-        self.weather_label.pack()
+        self.get_weather_button = ttk.Button(self, text="Debug Weather", command=self.debug_show_forecast_response, padding=30, bootstyle=('INFO', 'OUTLINE'))
+        self.get_weather_button.pack(pady=(50,0))
+
+
+    def create_weather_icons(self):
+
+        self.forecast0_image = Image.open(self.image_paths[0])
+        self.forecast0_image= self.forecast0_image.resize((128,128), Image.LANCZOS)
+        self.display0 = ImageTk.PhotoImage(self.forecast0_image)
+        self.forecast0_image_label = ttk.Label(self.forecast_frame, image=self.display0)
+        self.forecast0_image_label.grid(column=0, row=0, padx=5, pady=(5,1))
+
+        self.forecast1_image = Image.open(self.image_paths[1])
+        self.forecast1_image= self.forecast1_image.resize((128,128), Image.LANCZOS)
+        self.display1 = ImageTk.PhotoImage(self.forecast1_image)
+        self.forecast1_image_label = ttk.Label(self.forecast_frame, image=self.display1)
+        self.forecast1_image_label.grid(column=1, row=0, padx=5, pady=(5,1))
+
+        self.forecast2_image = Image.open(self.image_paths[2])
+        self.forecast2_image= self.forecast2_image.resize((128,128), Image.LANCZOS)
+        self.display2 = ImageTk.PhotoImage(self.forecast2_image)
+        self.forecast2_image_label = ttk.Label(self.forecast_frame, image=self.display2)
+        self.forecast2_image_label.grid(column=2, row=0, padx=5, pady=(5,1))
+
+
+    def attach_forecast_data(self):
+
+        self.condition_text0 = self.current_dict['current_condition_dict']['text']
+        self.condition_text1 = self.day1_dict['day_1_condition_dict']['text']
+        self.condition_text2 = self.day2_dict['day_2_condition_dict']['text']
+
+        self.temp0 = tk.StringVar(self,self.condition_text0 + ' ' + str(self.current_dict['current_temp_f']) + chr(176) + 'F')
+        self.temp1 = tk.StringVar(self,self.condition_text1 + ' ' + str(self.current_dict['current_temp_f']) + chr(176) + 'F')
+        self.temp2 = tk.StringVar(self,self.condition_text2 + ' ' + str(self.current_dict['current_temp_f']) + chr(176) + 'F')
+
+
+        self.forecast0 = ttk.Label(self.forecast_frame, textvariable=self.temp0)
+        self.forecast1 = ttk.Label(self.forecast_frame, textvariable=self.temp1)
+        self.forecast2 = ttk.Label(self.forecast_frame,textvariable=self.temp2)
+
+        self.forecast0.grid(column=0, row=1, pady=(1,10))
+        self.forecast1.grid(column=1, row=1, pady=(1,10))
+        self.forecast2.grid(column=2, row=1, pady=(1,10))
+
+
+    def debug_show_forecast_response(self):
+        wx.pp.pprint(self.forecast_response)
+        print(self.location)
+
 
 if __name__ == "__main__":
     app = WeatherApp()
--- weather_api.py
+++ weather_api.py
@@ -4,7 +4,7 @@ import pprint
 import urllib.request
 from PIL import Image
 
-pp = pprint.PrettyPrinter(compact=True)
+pp = pprint.PrettyPrinter(compact=True, sort_dicts=False)
 
 
 def realtime_api_call() -> dict:
@@ -20,14 +20,18 @@ def realtime_api_call() -> dict:
     response = requests.get(url, headers=headers, params=querystring)
 
     response = dict(response.json())
+    # might be deleted since the forecast api also contains current conditons
 
     return response
 
 
-def forecast_api_call():
+def forecast_api_call() -> dict:
+    """Makes a call to the forecast weather api for my location for 3 days of weather.
+    Returns a dict containing location info, current day weather, forecast weather for next 2 days"""
+
     url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
 
-    querystring = {"q":"97201","days":"1"}
+    querystring = {"q":"97201","days":"3"}
 
     headers = {
         "X-RapidAPI-Key": forecast_apikey,
@@ -38,18 +42,107 @@ def forecast_api_call():
 
     response = dict(response.json())
 
-    return response
+    # top-level dict contains location, current, forecast
+    # what values do I want?
+    # from location I want the name, region, and local time
+    loc_name = response['location']['name']
+    loc_region = response['location']['region']
+    loc_time = response['location']['localtime']
+    #print(loc_name,loc_region,loc_time)
 
-realtime_response = realtime_api_call()
+    # from current weather I want temp_f, is_day, condition dict, humidity
+    current_temp_f = response['current']['temp_f']
+    current_is_day = response['current']['is_day']
+    current_condition_dict = response['current']['condition']
+    current_humidity = response['current']['humidity']
+    #print(current_temp_f,current_is_day,current_condition_dict,current_humidity)
+
+    # forecast is split into 3 days due to API subscription limit
+    # day 0 is current day, so we can skip that and use 1 and 2
+    # from forecastday I want date, maxtemp_f, mintemp_f, avgtemp_f, condition dict, avghumidity 
+    # pp.pprint(response['forecast']['forecastday'][0]['date']) that's deep
+    day_1_maxtemp_f = response['forecast']['forecastday'][1]['day']['maxtemp_f']
+    day_1_mintemp_f = response['forecast']['forecastday'][1]['day']['mintemp_f']
+    day_1_avgtemp_f = response['forecast']['forecastday'][1]['day']['avgtemp_f']
+    day_1_condition_dict = response['forecast']['forecastday'][1]['day']['condition']
+    day_1_avghumidity = response['forecast']['forecastday'][1]['day']['avghumidity']
+    #print(day_1_maxtemp_f,day_1_mintemp_f,day_1_avgtemp_f,day_1_condition_dict,day_1_avghumidity)
+
+    day_2_maxtemp_f = response['forecast']['forecastday'][2]['day']['maxtemp_f']
+    day_2_mintemp_f = response['forecast']['forecastday'][2]['day']['mintemp_f']
+    day_2_avgtemp_f = response['forecast']['forecastday'][2]['day']['avgtemp_f']
+    day_2_condition_dict = response['forecast']['forecastday'][2]['day']['condition']
+    day_2_avghumidity = response['forecast']['forecastday'][2]['day']['avghumidity']
+    #print(day_2_maxtemp_f, day_2_mintemp_f, day_2_avgtemp_f,day_2_condition_dict,day_2_avghumidity)
+
+    # finally, dicts I can see in my bird brain
+    location_dict = {'loc_name':loc_name,
+                     'loc_region':loc_region,
+                     'loc_time':loc_time}
+    
+    current_dict =  {'current_temp_f':current_temp_f,
+                     'current_is_day':current_is_day,
+                     'current_condition_dict':current_condition_dict,
+                     'current_humidity':current_humidity}
+    
+    day_1_dict = {'day_1_maxtemp_f':day_1_maxtemp_f,
+                  'day_1_mintemp_f':day_1_mintemp_f,
+                  'day_1_avgtemp_f':day_1_avgtemp_f,
+                  'day_1_condition_dict':day_1_condition_dict,
+                  'day_1_avghumidity':day_1_avghumidity}
+    
+    day_2_dict = {'day_2_maxtemp_f':day_2_maxtemp_f,
+                  'day_2_mintemp_f':day_2_mintemp_f,
+                  'day_2_avgtemp_f':day_2_avgtemp_f,
+                  'day_2_condition_dict':day_2_condition_dict,
+                  'day_2_avghumidity':day_2_avghumidity}
+    
+    # one dict to rule them all
+    formatted_response = {'location_dict':location_dict,
+                          'current_dict':current_dict,
+                          'day_1_dict':day_1_dict,
+                          'day_2_dict':day_2_dict}
+
+    return formatted_response
+
+def get_condition_image(response_dict:dict) -> list:
+    """Takes in formatted api response dict, gets the images from the inner condition dicts, outputs relative image paths"""
+    
+    # select inner conditions dict for each day
+    current_condition_dict = response_dict['current_dict']['current_condition_dict']
+    day_1_condition_dict = response_dict['day_1_dict']['day_1_condition_dict']
+    day_2_condition_dict = response_dict['day_2_dict']['day_2_condition_dict']
+
+    # grab image urls
+    current_image = 'http:' + current_condition_dict['icon']
+    day1_image = 'http:' + day_1_condition_dict['icon']
+    day2_image = 'http:' + day_2_condition_dict['icon']
+
+    # download the images
+    urllib.request.urlretrieve(current_image,'currenticon.png')
+    urllib.request.urlretrieve(day1_image,'day1icon.png')
+    urllib.request.urlretrieve(day2_image,'day2icon.png')
+
+    current_image_path = 'currenticon.png'
+    day1_image_path = 'day1icon.png'
+    day2_image_path = 'day2icon.png'
+
+    return [current_image_path, day1_image_path, day2_image_path]
+
+#realtime_response = realtime_api_call()
 forecast_response = forecast_api_call()
 
+# silly me did not know I would become this evil
 # WHY ARE THERE SO MANY NESTED DICTS 
-for key,value in forecast_response.items():
-    if key == 'forecast':
-        for key,day in value.items():
-            print(day[0].keys())
-            for inner_key,info in day[0].items():
-                if inner_key == 'day':
-                    urllib.request.urlretrieve(('http:'+info['condition']['icon']),'weathericon.png')
-                    img = Image.open('weathericon.png')
-                    img.show()
+# for key,value in forecast_response.items():
+#     if key == 'forecast':
+#         for key,day in value.items():
+#             print(day[0].keys())
+#             for inner_key,info in day[0].items():
+#                 if inner_key == 'day':
+#                     urllib.request.urlretrieve(('http:'+info['condition']['icon']),'weathericon.png')
+#                     img = Image.open('weathericon.png')
+#                     #img.show()
+
+# image_paths = get_condition_image(forecast_response)
+# pp.pprint(image_paths)
